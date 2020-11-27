c335d8675b1e6f28e122baf37b62f4057d0a1e3e
Sergio Salazar <SergioSalazarOrdaz@gmail.com>
2020-11-27 09:27:24 -0600

    Journeys Component updated

--- src/app/journeys/journeys.component.ts
+++ src/app/journeys/journeys.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-journeys',
   templateUrl: './journeys.component.html',
   styleUrls: ['./journeys.component.css']
 })
-export class JourneysComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  export class AppComponent implements OnInit {
+    title = 'AngularCRUD';
+    users = [];
+    apiUrl = 'https://reqres.in/api';
+  
+    constructor(private http: HttpClient) {}
+  
+    ngOnInit() {
+      this.getUserById(2).subscribe(
+        val  =>{
+          alert(val.data.first_name)
+        }
+      )
+    }
+  
+    getUsers(): Observable<any> {
+      return this.http.get(this.apiUrl+"/users?page=2");
+    }
+  
+    getUserById(id:number): Observable<any> {
+      return this.http.get(this.apiUrl+"/users/"+id);
+    }
+  
+    clickedButton() {
+     this.users = [];
+      this.getUsers().subscribe(val => {
+        console.log(val.data);
+        val.data.forEach(element => {
+          this.users.push(element.first_name)
+        });
+      });
+  
+    }
+  
+  
   }
-
-}
