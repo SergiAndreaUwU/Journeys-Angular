4f1d52774b7212bdef98edc172923dce375b02bb
Sergio Salazar <SergioSalazarOrdaz@gmail.com>
2020-12-11 01:13:49 -0600

    testing writing on the fly

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+async function escribiendo(){
+  let i=0
+  let cadena="abcde";
+while(i<cadena.length){
+await  new Promise((res)=>{setTimeout(()=>{
+  this.cadenaInversa+=cadena.charAt(i)
+  i++;
+},1000)})
+}
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
+
 export class HomeComponent implements OnInit {
 
+  
   constructor() { }
-
+  cadenaInversa:string;
+  
+  
   ngOnInit(): void {
+    this.cadenaInversa=""
+    
+    
+    
+    escribiendo()
+    
+   
   }
+
+
 
 }
